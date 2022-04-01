b80ace37a76668e0809fd60b33a15b726f26db1d
hipporello <mustafa.sener@dealroomevents.com>
2022-04-01 13:56:21 +0300

    null state

--- src/storeMethods/useSyncState.ts
+++ src/storeMethods/useSyncState.ts
@@ -46,7 +46,7 @@ export default function useSyncState(
       newPath = immerPathToJsonPatchPath(immerPath); // immerPath.join('/');
       let potentialNewPath = findNonNumericPath(newPath);
       let stateAtPath = potentialNewPath === ""?store.getState(subtree):store.getStateAtPath(subtree, potentialNewPath);
-      let cmd = JSON.parse(JSON.stringify(stateAtPath));
+      let cmd = stateAtPath==null?{}:JSON.parse(JSON.stringify(stateAtPath));
       // @ts-ignore
       let targetPath = "/"+childKey;
       if(potentialNewPath !== newPath){
