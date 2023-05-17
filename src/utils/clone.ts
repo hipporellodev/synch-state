e49a3c32fffc941dc489b5c6b8780d7edf00cc34
hipporello <mustafa.sener@dealroomevents.com>
2023-05-18 00:38:53 +0300

    fast cloning

--- src/utils/clone.ts
+++ src/utils/clone.ts
@@ -1,5 +1,20 @@
-import {copy} from 'fastest-json-copy';
-export default function clone(data){
-  if(!data) return null;
-  return copy(data)
+export default function clone(obj){
+  let isArray = Array.isArray;
+  if (!obj) return obj;
+  if (isArray(obj)) {
+    let arr = [];
+    let length = obj.length;
+    for (let i = 0; i < length; i++) arr.push(clone(obj[i]))
+    return arr;
+  } else if (typeof obj === 'object') {
+    let keys = Object.keys(obj);
+    let length = keys.length;
+    let newObject = {};
+    for (let i = 0; i < length; i++) {
+      let key = keys[i];
+      newObject[key] = clone(obj[key]);
+    }
+    return newObject;
+  }
+  return obj;
 }
